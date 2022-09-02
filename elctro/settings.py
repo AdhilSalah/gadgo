bedd771f326b5958639782328f41232a8c6ff866
Adhil Salah <iamadhilsalah@gmail.com>
2022-09-02 14:51:07 +0530

    s3 v2

--- elctro/settings.py
+++ elctro/settings.py
@@ -179,7 +179,7 @@ DEFAULT_FILE_STORAGE = 'elctro.media_storages.MediaStorage'
 
 
 MEDIA_URL='/media/'
-
+MEDIA_ROOT=BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
